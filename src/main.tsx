cd3775a3f5dafb6d2b2af101cf6dfe27c3d38ef1
Alex <phoenix.trade2307@gmail.com>
2025-05-16 23:07:23 +0300

    trenning tasks

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
+import "./exercise.ts"
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>

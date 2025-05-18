8715159eb61481816d829ec36dc4e142e490a3ce
Alex <phoenix.trade2307@gmail.com>
2025-05-18 12:22:00 +0300

    added useForm

---
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,10 @@
+import styles from "./PageNotFound.module.css"
+
+export const PageNotFound = () => {
+  return (
+    <>
+      <h1 className={styles.title}>404</h1>
+      <h2 className={styles.subtitle}>page not found</h2>
+    </>
+  )
+}
---
+++ src/features/auth/ui/Login/Login.tsx
@@ -0,0 +1,68 @@
+import { useAppSelector } from "@/common/hooks"
+import { selectThemeMode } from "@/app/app-slice"
+import { getTheme } from "@/common/theme"
+import Grid from "@mui/material/Grid2"
+import { FormControl, FormControlLabel, FormGroup, FormLabel } from "@mui/material"
+import TextField from "@mui/material/TextField"
+import Checkbox from "@mui/material/Checkbox"
+import Button from "@mui/material/Button"
+import { useForm } from "react-hook-form"
+
+export const Login = () => {
+  const themeMode = useAppSelector(selectThemeMode)
+  const theme = getTheme(themeMode)
+  //
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+  })
+  type Inputs = {
+    email: string
+    password: string
+    rememberMe: boolean
+  }
+  //
+  return (
+    <Grid container justifyContent={"center"}>
+      <FormControl>
+        <FormLabel>
+          <p>
+            To login get registered
+            <a
+              style={{ color: theme.palette.primary.main, marginLeft: "5px" }}
+              href="https://social-network.samuraijs.com"
+              target={"_blank"}
+              rel={"noreferrer"}
+            >
+              here
+            </a>
+          </p>
+          <p>or use common test account credentials:</p>
+          <p>
+            <b>Email:</b> free@samuraijs.com
+          </p>
+          <p>
+            <b>Password:</b> free
+          </p>
+        </FormLabel>
+        <FormGroup>
+          <TextField label={"Email"} margin={"normal"} />
+          <TextField type={"password"} label={"Password"} margin={"normal"} />
+          <FormControlLabel control={<Checkbox />} label={"Remember me"} />
+          <Button type={"submit"} variant={"contained"} color={"primary"}>
+            Login
+          </Button>
+        </FormGroup>
+      </FormControl>
+    </Grid>
+  )
+}

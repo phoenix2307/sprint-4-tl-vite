7629a9209c20353a0660e5d26322eddabcbefb5a
Alex <phoenix.trade2307@gmail.com>
2025-05-18 18:31:48 +0300

    link as button to home page

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,10 +1,41 @@
 import styles from "./PageNotFound.module.css"
+//
+import { Link, LinkProps } from "@mui/material"
+import { Button, ButtonProps } from "@mui/material"
 
 export const PageNotFound = () => {
   return (
     <>
       <h1 className={styles.title}>404</h1>
       <h2 className={styles.subtitle}>page not found</h2>
+      <ButtonLink
+        href={"/"}
+        buttonProps={{
+          variant: "contained",
+        }}
+      >
+        Go to HomePage
+      </ButtonLink>
     </>
+  )
+}
+
+type ButtonLinkProps = LinkProps & {
+  buttonProps?: ButtonProps
+}
+
+const ButtonLink: React.FC<ButtonLinkProps> = ({ children, buttonProps = {}, ...linkProps }) => {
+  return (
+    <Link
+      {...linkProps}
+      underline={"none"}
+      sx={{
+        display: "inline-flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Button {...buttonProps}>{children}</Button>
+    </Link>
   )
 }

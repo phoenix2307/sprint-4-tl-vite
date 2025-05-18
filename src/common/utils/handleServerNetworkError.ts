92fbc52a6352ed72ac6334a55a0e27b98ef262de
Alex <phoenix.trade2307@gmail.com>
2025-05-18 17:51:34 +0300

    handling error zod

--- src/common/utils/handleServerNetworkError.ts
+++ src/common/utils/handleServerNetworkError.ts
@@ -1,16 +1,24 @@
 import { setAppErrorAC, setAppStatusAC } from "@/app/app-slice"
 import type { Dispatch } from "@reduxjs/toolkit"
 import axios from "axios"
+import { z } from "zod"
 
 export const handleServerNetworkError = (dispatch: Dispatch, error: unknown) => {
   let errorMessage
 
-  if (axios.isAxiosError(error)) {
-    errorMessage = error.response?.data?.message || error.message
-  } else if (error instanceof Error) {
-    errorMessage = `Native error: ${error.message}`
-  } else {
-    errorMessage = JSON.stringify(error)
+  switch (true) {
+    case axios.isAxiosError(error):
+      errorMessage = error.response?.data?.message || error.message
+      break
+    case error instanceof z.ZodError:
+      console.table(error.issues)
+      errorMessage = "Zod error. Look at the console"
+      break
+    case error instanceof Error:
+      errorMessage = `Native error: ${error.message}`
+      break
+    default:
+      errorMessage = JSON.stringify(error)
   }
 
   dispatch(setAppErrorAC({ error: errorMessage }))
